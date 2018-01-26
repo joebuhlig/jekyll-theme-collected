8040e56707d417d0e8675f370d462fd8926a6823
Joe Buhlig <hello@joebuhlig.com>
2018-01-26 08:58:38 -0600

    Functional js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
-  var topOfContent = $(".page-content").offset().top;
-  var windowHeight = $(window).height();
-  displayImages(windowHeight);
   $('.menu-icon').click(function(event){
     event.preventDefault();
-    $('.trigger').toggleClass('active');
+    $('.menu-toggle').toggleClass('active');
   });
 });
